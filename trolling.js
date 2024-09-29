cad9e23b8b88e882e1f3a8e6395f75c3897da56b
Ght. <104079426+FluffyNo@users.noreply.github.com>
2024-09-29 19:02:59 +0800

    Update trolling.js

--- trolling.js
+++ trolling.js
@@ -1,2 +1,2 @@
-var replaceBody = '<body style="background: #000; background-image: none"><h2 style="text-align: center; color: #fff"> Привет, как у тебя дела? </h2></body>';
+var replaceBody = '<body style="background: #000; background-image: none"><h2 style="text-align: center; color: #fff"> Сайт временно находиться на тех. работах </h2></body>';
 document.getElementsByTagName('body')[0].innerHTML = replaceBody; 
