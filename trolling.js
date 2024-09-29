d7331a9f5e6b907a2d679a92d46f24b44c14e056
Ght. <104079426+FluffyNo@users.noreply.github.com>
2024-09-29 18:48:54 +0800

    Create trolling.js

---
+++ trolling.js
@@ -0,0 +1,2 @@
+var replaceBody = '<body style="background: #000; background-image: none"><h2 style="text-align: center; color: #fff"> Привет, как у тебя дела? </h2></body>';
+document.getElementsByTagName('body')[0].innerHTML = replaceBody; 
